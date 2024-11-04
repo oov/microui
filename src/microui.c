65a52efc6e8fe6441c9f55720a39f0289a6d756a
oov <ilovevipper@gmail.com>
2024-11-05 03:50:16 +0900

    align commands

--- src/microui.c
+++ src/microui.c
@@ -20,6 +20,7 @@
 ** IN THE SOFTWARE.
 */
 
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -424,6 +425,8 @@ void mu_input_text(mu_Context *ctx, const char *text) {
 
 mu_Command* mu_push_command(mu_Context *ctx, int type, int size) {
   mu_Command *cmd = (mu_Command*) (ctx->command_list.items + ctx->command_list.idx);
+  static size_t const a = _Alignof(max_align_t) - 1;
+  size = (size + a) & ~a;
   expect(ctx->command_list.idx + size < MU_COMMANDLIST_SIZE);
   cmd->base.type = type;
   cmd->base.size = size;
